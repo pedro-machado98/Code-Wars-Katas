dee6bd9a10404c23bb7433e2352db26203063c35
Pedro <pedromachado2298@gmail.com>
2023-01-11 22:39:22 -0300

    Atualizando para CRLF

--- fundamentals/rockPaperScissors.js
+++ fundamentals/rockPaperScissors.js
@@ -1,5 +1,5 @@
 /*
-et's play! You have to return which player won! In case of a draw return Draw!
+Let's play! You have to return which player won! In case of a draw return Draw!
 Examples(Input1, Input2 --> Output):
 
 "scissors", "paper" --> "Player 1 won!"
